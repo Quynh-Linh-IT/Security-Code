7ca7a1e86d5e26597dc75b7131a86aa0c615e327
Quỳnh Linh <nguyenthanhquynhlinh@gmail.com>
2022-09-30 10:26:25 +0700

    Firsst

---
+++ BT3/Form1.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace BT3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }   
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.buttonXoa = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.buttonNhap = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn0 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnsave = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(432, 74);
+            this.panel1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(176, 17);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.PasswordChar = '*';
+            this.textBox1.Size = new System.Drawing.Size(240, 31);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Security Code";
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(36, 110);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(71, 36);
+            this.btn1.TabIndex = 1;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(146, 110);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(71, 36);
+            this.btn2.TabIndex = 2;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // buttonXoa
+            // 
+            this.buttonXoa.Location = new System.Drawing.Point(360, 110);
+            this.buttonXoa.Name = "buttonXoa";
+            this.buttonXoa.Size = new System.Drawing.Size(71, 36);
+            this.buttonXoa.TabIndex = 4;
+            this.buttonXoa.Text = "C";
+            this.buttonXoa.UseVisualStyleBackColor = true;
+            this.buttonXoa.Click += new System.EventHandler(this.buttonXoa_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(250, 110);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(71, 36);
+            this.btn3.TabIndex = 3;
+            this.btn3.Text = "3";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Location = new System.Drawing.Point(146, 171);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(71, 36);
+            this.btn5.TabIndex = 6;
+            this.btn5.Text = "5";
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Location = new System.Drawing.Point(36, 171);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(71, 36);
+            this.btn4.TabIndex = 5;
+            this.btn4.Text = "4";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // buttonNhap
+            // 
+            this.buttonNhap.Location = new System.Drawing.Point(360, 171);
+            this.buttonNhap.Name = "buttonNhap";
+            this.buttonNhap.Size = new System.Drawing.Size(71, 36);
+            this.buttonNhap.TabIndex = 8;
+            this.buttonNhap.Text = "Enter";
+            this.buttonNhap.UseVisualStyleBackColor = true;
+            this.buttonNhap.Click += new System.EventHandler(this.buttonNhap_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Location = new System.Drawing.Point(250, 171);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(71, 36);
+            this.btn6.TabIndex = 7;
+            this.btn6.Text = "6";
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.Location = new System.Drawing.Point(146, 234);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(71, 36);
+            this.btn8.TabIndex = 10;
+            this.btn8.Text = "8";
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.Location = new System.Drawing.Point(36, 234);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(71, 36);
+            this.btn7.TabIndex = 9;
+            this.btn7.Text = "7";
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            // 
+            // btn0
+            // 
+            this.btn0.Location = new System.Drawing.Point(360, 234);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(71, 36);
+            this.btn0.TabIndex = 12;
+            this.btn0.Text = "0";
+            this.btn0.UseVisualStyleBackColor = true;
+            this.btn0.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.Location = new System.Drawing.Point(250, 234);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(71, 36);
+            this.btn9.TabIndex = 11;
+            this.btn9.Text = "9";
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 294);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(431, 134);
+            this.listBox1.TabIndex = 13;
+            // 
+            // btnsave
+            // 
+            this.btnsave.Location = new System.Drawing.Point(146, 434);
+            this.btnsave.Name = "btnsave";
+            this.btnsave.Size = new System.Drawing.Size(148, 46);
+            this.btnsave.TabIndex = 14;
+            this.btnsave.Text = "Save";
+            this.btnsave.UseVisualStyleBackColor = true;
+            this.btnsave.Click += new System.EventHandler(this.btnsave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(456, 492);
+            this.Controls.Add(this.btnsave);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btn0);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.buttonNhap);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.buttonXoa);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button buttonXoa;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button buttonNhap;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnsave;
+    }
+}
+
---
+++ BT3/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BT3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "1";
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "2";
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "3";
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "4";
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "5";
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "6";
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "7";
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "8";
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "9";
+        }
+
+        private void buttonXoa_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+        }
+
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "0";
+        }
+
+        private void buttonNhap_Click(object sender, EventArgs e)
+        {
+            List<int> listAll = new List<int>() { 1645, 1689, 8345, 9998, 1006, 1008 };
+            List<int> listScientist = new List<int>() { 9998, 1006, 1008 };
+            List<int> listCustodians = new List<int>() { 8345 };
+            List<int> listTechnicians = new List<int>() { 1645, 1689 };
+            int number = Convert.ToInt32(textBox1.Text);
+            DateTime aDateTime = DateTime.Now;
+            if (listAll.Contains(number))
+            {
+                foreach (int i in listScientist)
+                {
+                    if (i == number)
+                    {
+                        listBox1.Items.Add(aDateTime + " " + "Scientist");
+                    }
+                }
+                foreach (int i in listCustodians)
+                 {
+                    if (i == number)
+                     {
+                        listBox1.Items.Add(aDateTime + " " + "Custodians");
+                     }
+
+                 }
+                foreach (int i in listTechnicians)
+                 {
+                    if (i == number)
+                     {
+                          listBox1.Items.Add(aDateTime + " " + "Technicians");
+                     }
+                 }
+            } else{
+                listBox1.Items.Add(aDateTime + " " + "Restricted Access!");
+            }  
+        }
+
+        private void btnsave_Click(object sender, EventArgs e)
+        {
+            string path = "";
+            StreamWriter streamWriter;
+            path = "../TextFile4.txt";
+            if (!File.Exists(path))
+            {
+                streamWriter = new StreamWriter(path);
+                foreach(string item in listBox1.Items)
+                {
+                    streamWriter.WriteLine(item);
+                }
+                MessageBox.Show("Save", "Lưu file", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                streamWriter.Close();
+            } else
+            {
+                streamWriter = File.AppendText(path);
+                foreach (string item in listBox1.Items)
+                {
+                    streamWriter.WriteLine(item);
+                }
+                MessageBox.Show("Lưu thành công","Lưu file",MessageBoxButtons.OK,MessageBoxIcon.Exclamation);
+            }
+        }
+    }
+}
